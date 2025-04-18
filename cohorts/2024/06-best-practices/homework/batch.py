a35b03f21d0bd3be486fad0053fbef4b8a6529da
Vishal Thakur <33867147+vt-dataengineer@users.noreply.github.com>
2025-04-18 13:26:39 +0000

    updated

--- cohorts/2024/06-best-practices/homework/batch.py
+++ cohorts/2024/06-best-practices/homework/batch.py
@@ -2,51 +2,84 @@
 # coding: utf-8
 
 import sys
+import os
 import pickle
 import pandas as pd
 
 
-year = int(sys.argv[1])
-month = int(sys.argv[2])
+# year = int(sys.argv[1])
+# month = int(sys.argv[2])
 
-input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-
-
-with open('model.bin', 'rb') as f_in:
-    dv, lr = pickle.load(f_in)
-
-
-categorical = ['PULocationID', 'DOLocationID']
-
-def read_data(filename):
-    df = pd.read_parquet(filename)
-    
+def prepare_data(df, categorical):
     df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
 
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
-    
+
     return df
 
 
-df = read_data(input_file)
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+def read_data(filename, categorical):
+    S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
+
+    if S3_ENDPOINT_URL is not None:
+        options = {
+            'client_kwargs': {
+                'endpoint_url': S3_ENDPOINT_URL
+            }
+        }
+
+        df = pd.read_parquet(filename, storage_options=options)
+        # df = pd.read_parquet('s3://bucket/file.parquet', storage_options=options)
+    else:
+        df = pd.read_parquet(filename)
+
+    return prepare_data(df, categorical)
+
+def get_input_path(year, month):
+    default_input_pattern = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    input_pattern = os.getenv('INPUT_FILE_PATTERN', default_input_pattern)
+    return input_pattern.format(year=year, month=month)
 
 
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = lr.predict(X_val)
+def get_output_path(year, month):
+    default_output_pattern = 's3://nyc-duration-prediction-alexey/taxi_type=fhv/year={year:04d}/month={month:02d}/predictions.parquet'
+    output_pattern = os.getenv('OUTPUT_FILE_PATTERN', default_output_pattern)
+    return output_pattern.format(year=year, month=month)
 
 
-print('predicted mean duration:', y_pred.mean())
+def main(year, month):
+    # input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    # output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'  
+
+    input_file = get_input_path(year, month)
+    output_file = get_output_path(year, month)
+
+    categorical = ['PULocationID', 'DOLocationID']
+    
+    df = read_data(input_file, categorical)
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+
+    with open('model.bin', 'rb') as f_in:
+        dv, lr = pickle.load(f_in)
+        
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = lr.predict(X_val)
+    
+    print('predicted mean duration:', y_pred.mean())
+    
+    df_result = pd.DataFrame()
+    df_result['ride_id'] = df['ride_id']
+    df_result['predicted_duration'] = y_pred
+    
+    df_result.to_parquet(output_file, engine='pyarrow', index=False)
 
 
-df_result = pd.DataFrame()
-df_result['ride_id'] = df['ride_id']
-df_result['predicted_duration'] = y_pred
+if __name__ == '__main__':
+    year = int(sys.argv[1])
+    month = int(sys.argv[2])
 
-
-df_result.to_parquet(output_file, engine='pyarrow', index=False)
+    main(year=year, month=month)
